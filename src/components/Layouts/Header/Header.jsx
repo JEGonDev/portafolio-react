a35a0499c1860ec4b6b0957764e00a23f1f56d46
JuanitoZaZa <juanestebangonzalezcaro567@gmail.com>
2024-10-10 11:50:30 -0500

    Primer commit, se crea el proyecto, la estructura de carpetas y los componentes que haran parte del header

---
+++ src/components/Layouts/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '../../UI/Navbar/Navbar'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <header className='header'>
+        <h1>Juan Esteban Gonzalez Caro</h1>
+        <Navbar/>
+    </header> 
+  )
+}
---
+++ src/components/UI/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { ItemNavBar } from '../ItemNavBar/ItemNavBar'
+import './Navbar.css'
+
+export const Navbar = () => {
+  return (
+    <nav>
+    <ul>
+        <ItemNavBar text='¿Quien soy?'/>
+        <ItemNavBar text='Un saluo'/>
+        <ItemNavBar text='FC Barcelona'/>
+        <ItemNavBar/>
+    </ul>
+    </nav>
+  )
+}
